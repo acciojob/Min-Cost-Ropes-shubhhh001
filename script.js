cb62c13e1503259679b54c5a633e8591bc463f5f
shubhhh001 <160817878+shubhhh001@users.noreply.github.com>
2026-05-16 16:33:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function mincost(arr)
 { 
     let minCost=0;
 	while(arr.length>1){
-		arr.sort();
+		arr.sort((a,b)=> a - b);
 		let x=arr.shift();
 		let y=arr.shift();
 
 		let sum=x+y;
-		mincost+=sum;
+		minCost+=sum;
 		arr.push(sum);
 	}
 	return minCost;
